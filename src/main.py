5f0c8a1fe01ecd633249c64437472350f3eba83a
Matthew William Blanckenberg <27007707@sun.ac.za>
2026-08-12 12:00:19 +0000

    Implemented MongoDB online database

--- src/main.py
+++ src/main.py
@@ -1,46 +1,79 @@
+# Default libraries + Dotenv
 import os
 import sys
 from dotenv import load_dotenv
-
+load_dotenv()
+# Discord libs
 import discord
 from discord.ext import commands
 from discord import app_commands
+# MongoDB
+import motor.motor_asyncio
+# Scripts
 from backlog_modal import BackLog
 import backlog_printer
 from view_messages import DeleteBackLog
-
 import debugger as DB
+
 
 # Debug flag
 DEBUG = True
 DB.set_debug(DEBUG)
 
-# Loading discord token from .env file
-load_dotenv()
+
 
 '''
 Main bot class
 '''
 class Client(commands.Bot):
 
-    # Register the Cog stuff
-    async def setup_hook(self): 
+    # Run pre-ready processes.
+    async def setup_hook(self):
+        # Database setup
+        Password = os.getenv("MONGO_DB_PASSWORD")
+        Username = os.getenv("MONGO_DB_USERNAME")
+        uri = f"mongodb+srv://{Username}:{Password}@hansdb.s0wgibr.mongodb.net/?appName=HansDB"
+        self.mongo_client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self.mongo_client["HansDB"]
+
         await self.load_extension("voice_cog")
-        #await self.load_extension("backlog_modal")
         DB.Debug("Loaded VoiceCog extension successfully.")
 
     # Runs when the bot turns on
     async def on_ready(self):
         DB.Debug(f'Logged on as {self.user}!')
 
-        # Force load slash commands
+        # Force load slash commands for every guild avaliable
         try:
-            guild = discord.Object(id=os.getenv("DISCORD_SERVER_ID"))
-            self.tree.copy_global_to(guild=guild)
-            synced = await self.tree.sync(guild=guild)
-            DB.Debug(f'Synced {len(synced)} commands to guild {guild.id}')
+            for guild in self.guilds:
+                guild_obj = discord.Object(id=guild.id)
+                self.tree.copy_global_to(guild=guild_obj)
+                synced = await self.tree.sync(guild=guild_obj)
+                DB.Debug(f'Synced {len(synced)} commands to guild {guild.name} {guild.id}')
+                # If we ever get more than persay 5 servers, do synced = await self.tree.sync() for global sync
+            
         except Exception as e:
             DB.Debug(f'Error syncing commands: {e}')
+
+        # Ensure all servers we are on are registered
+        # If not, register them.
+        collection = self.db["server_settings"]
+        for guild in self.guilds:
+            default_settings = {
+                "guild_id"              : str(guild.id),
+                "guild_name"            : guild.name,
+                "welcome_channel_id"    : None,
+                "log_channel_id"        : None,
+                "joined_date"           : guild.me.joined_at.isoformat()
+            }
+
+            await collection.update_one(
+                {"guild_id": str(guild.id)},
+                # $setOnInsert doesn't overwrite old settings if they exist.
+                {"$setOnInsert": default_settings},
+                upsert=True
+            )
+        print("Checked and initilised all server settings.")
 
 
     # When a message is sent in the server, print the content and sender
@@ -64,6 +97,33 @@ class Client(commands.Bot):
         # channel = bot.get_channel(CHANNEL_WELCOME)
         # await channel.send(f"Welcome to the server, {member.mention}!")
         DB.Debug(f'Member joined: {member}')
+
+    # Close mongo_DB connections
+    async def close(self):
+        self.mongo_client.close()
+        await super().close()
+
+    # Add new servers contents to MONGO
+    async def on_guild_join(self, guild: discord.Guild):
+        collection = self.db["server_settings"]
+
+        # Set the default settings of every server.
+        default_settings = {
+            "guild_id"              : str(guild.id),
+            "guild_name"            : guild.name,
+            "welcome_channel_id"    : None,
+            "log_channel_id"        : None,
+            "joined_date"           : guild.me.joined_at.isoformat()
+        }
+
+        await collection.update_one(
+            {"guild_id": str(guild.id)},
+            # $setOnInsert doesn't overwrite old settings if they exist.
+            {"$setOnInsert": default_settings},
+            upsert=True
+        )
+
+        print(f"Initialized settings for new server: {guild.name} ({guild.id})")
 
 
 intents = discord.Intents.default()
